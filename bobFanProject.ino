6e2293b0c9fcbaf4c08bc2deca5a1b5ae003caf2
Devaroop <devaroop123@yahoo.co.in>
2019-09-26 09:30:47 +0530

    add lm35 sensor

--- bobFanProject.ino
+++ bobFanProject.ino
@@ -6,9 +6,17 @@
 //#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
+//https://github.com/schinken/SimpleTimer
+#include <SimpleTimer.h>
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "xxxxxxxxxxxxxxxxxxxxxx";
+char auth[] = "xxxxxxxxxxxxxxxxxxx";
+
+float vref = 3.3;
+float resolution = vref / 1023.0;
+float temperature;
+
+SimpleTimer timer;
 
 BLYNK_CONNECTED() {
   // Request Blynk server to re-send latest values for all pins
@@ -22,10 +30,22 @@ void setup()
   digitalWrite(13, HIGH);
   digitalWrite(14, HIGH);
   digitalWrite(15, HIGH);
-  Blynk.begin(auth, "yyyyyyy", "xxxxxxx");
+  Blynk.begin(auth, "xxxxxxxxxxx", "yyyyyyyyyyyyyy");
+  timer.setInterval(60000, sendUptime);
+}
+
+void sendUptime()
+{
+  float temperature = analogRead(A0);//lm35 is connected to pin A0 on NodeMcu
+  temperature = (temperature * resolution);
+  temperature = temperature * 100.0;
+  //Serial.print("LM35 temperature: ");//serial print the value 
+  //Serial.println(temperature);
+  Blynk.virtualWrite(V0, temperature);//send the value to blynk application
 }
 
 void loop()
 {
   Blynk.run();
+  timer.run();
 }
